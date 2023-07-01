48e3c4769dcc7f520cd8204611f937206341f15a
Bibhuti <biswalbibhuti8@gmail.com>
2023-07-01 19:20:17 +0530

    Added link to other pages in home screen

---
+++ script.js
@@ -0,0 +1,9 @@
+const loginBtn=document.getElementById('login');
+const signupBtn=document.getElementById('signup');
+
+loginBtn.addEventListener('click',()=>{
+    location.href='./Login/index.html';
+})
+signupBtn.addEventListener('click',()=>{
+    location.href='./Signup/index.html';
+})
